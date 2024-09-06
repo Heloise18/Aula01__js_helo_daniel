ef41b49ccce6838daa842d91a10c5873bb0d616c
Heloise18 <heloisezelma18@gmail.com>
2024-09-06 09:02:49 -0300

    Add files via upload

---
+++ AULA01_JS_DANIEL_HELO/1-meuPrimeiroPrograma.js
@@ -0,0 +1,17 @@
+//primeira instrução em JS
+
+console.log("\n Olá mundo cruel da programação\n");
+const aluno1="Heloise";
+const aluno2="Daniel";
+var aluno3="eu"
+let aluno4="voce"
+
+const idadeAluno1 = 16;
+const idadeAluno2 = 15;
+
+const somaIdades = idadeAluno1+idadeAluno2;
+
+const dupla = aluno1+" e " +aluno2+"";
+
+console.log("somos a dupla: "+dupla+"\n" );
+console.log("A soma de nossas idades é: " +somaIdades+" anos ");
